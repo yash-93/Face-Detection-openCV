3742250dabb003e5aebfee0e5abde205ccc6fb96
yash-93 <yashdeepbachhas93@gmail.com>
2020-02-17 13:47:25 +0530

    Detecting frontal face and creating a rectangle around that

---
+++ faces.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        print(x, y, w, h)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+        img_item = "my-image.png"
+        cv2.imwrite(img_item, roi_gray)
+        color = (0, 255, 0)
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
