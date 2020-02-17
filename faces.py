b522ddccac1ca5472cf1e862d588395dc5c91397
yash-93 <yashdeepbachhas93@gmail.com>
2020-02-17 15:42:49 +0530

    Update faces.py
    Implemented Recognizer

--- faces.py
+++ faces.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
 
 cap = cv2.VideoCapture(0)
 
@@ -10,9 +12,14 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
     for (x, y, w, h) in faces:
-        print(x, y, w, h)
+        # print(x, y, w, h)
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = frame[y:y + h, x:x + w]
+
+        id_, conf = recognizer.predict(roi_gray)
+        if conf>=45 and conf<=85:
+            print(id_)
+
         img_item = "my-image.png"
         cv2.imwrite(img_item, roi_gray)
         color = (0, 255, 0)
