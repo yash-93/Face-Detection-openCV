a8193426aa760c6f80f3afd7148760f85980de03
yash-93 <yashdeepbachhas93@gmail.com>
2020-02-17 14:59:19 +0530

    added labels from directories

---
+++ faces-train.py
@@ -0,0 +1,41 @@
+import cv2
+import os
+from PIL import Image
+import numpy as np
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, "images")
+
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+
+current_id = 0
+label_ids = {}
+y_labels = []
+x_train = []
+
+for root, dirs, files in os.walk(image_dir):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg"):
+            path = os.path.join(root, file)
+            # label = os.path.basename(os.path.dirname(path)).replace(" ", "-").lower()
+            label = os.path.basename(root).replace(" ", "-").lower()
+            print(label, path)
+            if not label in label_ids:
+                label_ids[label] = current_id
+                current_id += 1
+
+            id_ = label_ids[label]
+            print(label_ids)
+            # y_labels.append(label)
+            # x_train.append(path)
+            pil_image = Image.open(path).convert("L")
+            image_array = np.array(pil_image, "uint8")
+            print(image_array)
+            faces = face_cascade.detectMultiScale(image_array)
+
+            for (x, y, w, h) in faces:
+                roi = image_array[y:y+h, x:x+w]
+                x_train.append(roi)
+                y_labels.append(id_)
+# print(y_labels)
+# print(x_train)
