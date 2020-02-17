b69b2b22198ddac68b13c4614cbd227317d4648c
yash-93 <yashdeepbachhas93@gmail.com>
2020-02-17 15:31:33 +0530

    Training openCV Recognizer

--- faces-train.py
+++ faces-train.py
@@ -2,11 +2,13 @@ import cv2
 import os
 from PIL import Image
 import numpy as np
+import pickle
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 current_id = 0
 label_ids = {}
@@ -19,18 +21,18 @@ for root, dirs, files in os.walk(image_dir):
             path = os.path.join(root, file)
             # label = os.path.basename(os.path.dirname(path)).replace(" ", "-").lower()
             label = os.path.basename(root).replace(" ", "-").lower()
-            print(label, path)
+            # print(label, path)
             if not label in label_ids:
                 label_ids[label] = current_id
                 current_id += 1
 
             id_ = label_ids[label]
-            print(label_ids)
+            # print(label_ids)
             # y_labels.append(label)
             # x_train.append(path)
             pil_image = Image.open(path).convert("L")
             image_array = np.array(pil_image, "uint8")
-            print(image_array)
+            # print(image_array)
             faces = face_cascade.detectMultiScale(image_array)
 
             for (x, y, w, h) in faces:
@@ -39,3 +41,8 @@ for root, dirs, files in os.walk(image_dir):
                 y_labels.append(id_)
 # print(y_labels)
 # print(x_train)
+with open("labels.pickle", 'wb') as f:
+    pickle.dump(label_ids, f)
+
+recognizer.train(x_train, np.array((y_labels)))
+recognizer.save("trainner.yml")
