9211baadab6e6859043bdebd11038ad026044af5
yash-93 <yashdeepbachhas93@gmail.com>
2020-04-06 12:37:21 +0530

    update

--- webcamFaces.py
+++ webcamFaces.py
@@ -3,16 +3,16 @@ import numpy as np
 import pickle
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
-eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
-smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_smile.xml')
+# eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
+# smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_smile.xml')
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read("trainner.yml")
 
-labels = {}
+# labels = {}
 with open("labels.pickle", 'rb') as f:
     og_labels = pickle.load(f)
-    labels = {v:k for k,v in og_labels.items()}
+    labels = {v: k for k, v in og_labels.items()}
 
 cap = cv2.VideoCapture(0)
 
@@ -42,13 +42,13 @@ while True:
         end_cord_x = x + w
         end_cord_y = y + h
         cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), stroke)
-
-        smile = smile_cascade.detectMultiScale(roi_gray)
-        for (sx, sy, sw, sh) in smile:
-            cv2.rectangle(roi_color, (sx, sy), (sx + sw, sy + sh), (255, 255, 0), stroke)
+        # eyes = eye_cascade.detectMultiScale(roi_gray)
+        # for (ex,ey,ew,eh) in eyes:
+        #     cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), stroke)
+        #
+        # smile = smile_cascade.detectMultiScale(roi_gray)
+        # for (sx, sy, sw, sh) in smile:
+        #     cv2.rectangle(roi_color, (sx, sy), (sx + sw, sy + sh), (255, 255, 0), stroke)
 
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
